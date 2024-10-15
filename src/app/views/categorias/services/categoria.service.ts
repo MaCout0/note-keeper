36dbc4c9929d133d0334f8cb88648268b2a4edd6
MaCout0 <jkmbint@gmail.com>
2024-10-15 13:56:36 -0300

    Pequenas alterações

--- src/app/views/categorias/services/categoria.service.ts
+++ src/app/views/categorias/services/categoria.service.ts
@@ -1,11 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ListagemCategoria } from '../models/categorias.models';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
+  
   private readonly url = '&{environment.API_URL}/categorias';
 
   constructor(private http: HttpClient) { }
+
+  selecionarTodos(): Observable<ListagemCategoria[]> {
+    return this.http.get<ListagemCategoria[]>(this.url);
+  }
 }
